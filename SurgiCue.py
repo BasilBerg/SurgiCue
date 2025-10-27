43c2ca864e66b2fed2537f5f89ab968ad75a231c
BasilBerg <vonpapenole@gmail.com>
2025-10-27 22:35:58 +0100

    implement icons

--- SurgiCue.py
+++ SurgiCue.py
@@ -1,10 +1,13 @@
+import os
 import tkinter
 import time
 from enum import Enum, auto
 
+from PIL import Image, ImageTk
+
 FPS = 60
 
-BACKGROUND_COLOR = '#EEEEEE'  # TODO: change to #FFFFFF
+BACKGROUND_COLOR = '#BBBBBB'  # TODO: change to #FFFFFF
 COLOR = '#00FF00'
 ERASER_COLOR = '#FFFFFF'
 LONG_PRESS_DURATION = 0.5
@@ -15,6 +18,11 @@ UI_LINE_WIDTH = 5
 DRAWING_WIDTH = 10
 ERASER_WIDTH = 30
 LINE_WIDTH = 10
+
+ICON_DIRECTORY = 'icons/'
+ICON_SIZE = 100
+ICON_TINT = '#FF0000'
+ICON_DURATION = 0.5
 
 
 def get_current_time():
@@ -44,7 +52,15 @@ class SurgiCue:
     def __init__(self):
         self.root = tkinter.Tk()
         self.root.title('SurgiCue')
-        self.root.attributes('-fullscreen', False)
+        self.root.attributes('-fullscreen', True)
+        try:
+            icon_path = os.path.join(ICON_DIRECTORY, 'window-icon.png')
+            self._win_icon = tkinter.PhotoImage(file=icon_path)
+            self.root.iconphoto(True, self._win_icon)
+        except Exception as e:
+            print(f"[WARNING] Icon not found ({icon_path}): {e}")
+
+
 
         self.w = self.root.winfo_screenwidth()
         self.h = self.root.winfo_screenheight()
@@ -64,6 +80,8 @@ class SurgiCue:
         self.line_start_coordinates = None
         self.line_end_coordinates = None
         self.line_preview_id = None
+
+        self.last_action_icon_time = 0
 
         self.root.bind('<Escape>', lambda e: self.root.destroy())
         self.canvas.bind('<ButtonPress-1>', self.handle_clicks('left_pressed'))
@@ -208,6 +226,7 @@ class SurgiCue:
     def perform_state_actions(self):
         self.canvas.delete('overlay')
 
+        icon_filename = ''
         x, y = self.pointer_coordinates
 
         if (self.line_end_coordinates != None):
@@ -216,22 +235,26 @@ class SurgiCue:
 
         match self.state:
             case States.POINTER:
+                # icon_filename = 'pointer.png'
                 half = POINTER_SIZE // 2
                 self.canvas.create_line(x - half, y, x + half, y, fill=COLOR, width=UI_LINE_WIDTH,
                                         tags=('overlay', 'pointer'))
                 self.canvas.create_line(x, y - half, x, y + half, fill=COLOR, width=UI_LINE_WIDTH,
                                         tags=('overlay', 'pointer'))
                 self.last_draw_point = None
-            case States.DRAW:
 
+            case States.DRAW:
+                icon_filename = 'draw.png'
                 self.draw_rectangle(COLOR, COLOR, DRAWING_WIDTH, UI_LINE_WIDTH, x, y)
                 self.draw(COLOR, DRAWING_WIDTH, x, y)
 
             case States.ERASE:
+                icon_filename = 'erase.png'
                 self.draw_rectangle(ERASER_COLOR, COLOR, ERASER_WIDTH, UI_LINE_WIDTH, x, y)
                 self.draw(ERASER_COLOR, ERASER_WIDTH, x, y)
 
             case States.LINE:
+                icon_filename = 'line.png'
                 self.draw_rectangle(COLOR, COLOR, DRAWING_WIDTH, UI_LINE_WIDTH, x, y)
 
                 if self.line_start_coordinates is None:
@@ -242,9 +265,15 @@ class SurgiCue:
                                         tags=('overlay', 'line_preview'))
 
             case States.UNDO:
+                self.last_action_icon_time = get_current_time()
+                icon_filename = 'undo.png'
+                # TODO implement
+
                 self.state = States.POINTER
 
             case States.CLEAR:
+                self.last_action_icon_time = get_current_time()
+                icon_filename = 'clear.png'
                 self.canvas.delete('drawn')
                 self.state = States.POINTER
 
@@ -259,6 +288,8 @@ class SurgiCue:
             if (start_x, start_y) != (end_x, end_y):
                 self.canvas.create_line(start_x, start_y, end_x, end_y, fill=COLOR, width=LINE_WIDTH, tags=('drawn',))
             self.line_start_coordinates = None
+
+        self.display_icon(icon_filename)
 
     def draw(self, color: str, line_width: int, x: int, y: int):
         if self.last_draw_point is None:
@@ -278,13 +309,36 @@ class SurgiCue:
                                      tags=('overlay', 'pointer'))
 
     def loop(self):
-
         self.perform_state_actions()
-
         self.root.after(1000 // FPS, self.loop)
 
     def run(self):
         self.root.mainloop()
+
+    def display_icon(self, filename: str):
+        if (filename == ''):
+            if (get_current_time() - self.last_action_icon_time > ICON_DURATION):
+                self.canvas.delete('icon')
+                self.last_action_icon_time = 0
+            return
+
+        icon_position = (10, 10)
+
+        try:
+            icon_path = os.path.join(ICON_DIRECTORY, filename)
+            icon = Image.open(icon_path).convert("RGBA")
+            resized_icon = icon.resize((ICON_SIZE, ICON_SIZE))
+            alpha_channel = resized_icon.getchannel('A')
+            tinted_icon = Image.new("RGBA", resized_icon.size, ICON_TINT)
+            tinted_icon.putalpha(alpha_channel)
+            self.icon_image = ImageTk.PhotoImage(tinted_icon)
+            self.canvas.create_image(icon_position, anchor="nw", image=self.icon_image, tags=('icon'))
+
+        except Exception as e:
+            print(f"[WARNING] Icon not found ({icon_path}): {e}")
+            toolname = filename.split(".")[0]
+            self.canvas.create_text(icon_position, anchor="nw", text=toolname, fill=ICON_TINT,
+                                    font=("Arial", ICON_SIZE // 5, "bold"), tags=('icon'))
 
 
 if __name__ == '__main__':
